94ffcfc0cd56484e3891d17f78056f15b81e782d
Rodrigo Chavez <chavezrodrigo001@gmail.com>
2026-07-19 22:11:41 -0300

    add flake.nix for nix packaging

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "An mpv script to fetch Japanese subtitles using jimaku.cc";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f nixpkgs.legacyPackages.${system});
+    in {
+      packages = forEachSystem (pkgs: {
+        default = pkgs.stdenv.mkDerivation {
+          pname = "jimaku-sub-fetcher";
+          version = "1.0.0";
+          
+          src = ./.;
+
+          dontBuild = true;
+
+          propagatedUserEnvPkgs = [
+            pkgs.curl
+            pkgs.python3Packages.ffsubsync
+            pkgs.python3Packages.guessit
+          ];
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/share/mpv/scripts
+            cp -r * $out/share/mpv/scripts/
+            rm -f $out/share/mpv/scripts/flake.nix
+
+            mkdir -p $out/bin
+            cat <<- 'EOF' > $out/bin/jimaku-setup
+            	#!/usr/bin/env bash
+            	echo "Linking jimaku-sub-fetcher to your mpv config..."
+            	mkdir -p $HOME/.config/mpv/scripts
+            	ln -sf $HOME/.nix-profile/share/mpv/scripts/* $HOME/.config/mpv/scripts/
+            	echo "Done! jimaku-sub-fetcher is now active in mpv."
+            	EOF
+            
+            chmod +x $out/bin/jimaku-setup
+
+            runHook postInstall
+          '';
+        };
+      });
+    };
+}
